b2e8bf169d3d6cdbb9a28d0cf22a9ad99fd221f7
Noam Saar <noamsaar11@gmail.com>
2023-11-15 16:43:50 +0200

    Add getRandomColor() to util

--- js/util.js
+++ js/util.js
@@ -1,7 +1,17 @@
 'use strict'
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
-  }  
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomColor() {
+    // Generate a random hex color code
+    const letters = '0123456789ABCDEF';
+    let color = '#'
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color
+}

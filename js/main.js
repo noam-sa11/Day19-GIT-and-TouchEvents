8532ccaee4f772e57a4ceddba048e51f656faa80
Noam Saar <noamsaar11@gmail.com>
2023-11-15 16:24:16 +0200

    Add ball size limit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,10 @@ var gDiameter = +document.querySelector('.ball').innerHTML
 function onBallClick() {
     gDiameter += 50
 
+    if (gDiameter > 400) {
+        gDiameter = 100
+    }
+
     const elBall = document.querySelector('.ball')
 
     elBall.style.width = gDiameter + 'px'
